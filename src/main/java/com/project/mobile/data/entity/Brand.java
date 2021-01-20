76ec86a2dfcfb22d873ee816ce12cf35d8ff7fc4
Vasil <vvpanayotovv@gmail.com>
2021-01-20 12:08:34 +0200

    initial commit

---
+++ src/main/java/com/project/mobile/data/entity/Brand.java
@@ -0,0 +1,33 @@
+package com.project.mobile.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Instant created;
+
+    @Column(nullable = false)
+    private Instant modified;
+
+}
