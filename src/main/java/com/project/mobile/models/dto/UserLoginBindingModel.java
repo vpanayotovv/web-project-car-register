dfc445446a64f7dd330eb1be37de72a8e5117a6f
Vasil <vvpanayotovv@gmail.com>
2021-01-31 12:24:46 +0200

    refactor login

--- src/main/java/com/project/mobile/models/dto/UserLoginBindingModel.java
+++ src/main/java/com/project/mobile/models/dto/UserLoginBindingModel.java
@@ -3,6 +3,9 @@ package com.project.mobile.models.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
--- src/main/java/com/project/mobile/web/LoginController.java
+++ src/main/java/com/project/mobile/web/LoginController.java
@@ -1,11 +1,18 @@
 package com.project.mobile.web;
 
 import com.project.mobile.models.dto.UserLoginBindingModel;
+import com.project.mobile.models.entity.User;
 import com.project.mobile.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -19,18 +26,28 @@ public class LoginController {
 
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model) {
+        if (!model.containsAttribute("userLoginBindingModel")){
+            model.addAttribute("notFound",false);
+        }
         return "auth-login";
     }
 
     @PostMapping("/login")
-    public String loginConfirm(UserLoginBindingModel userLoginBindingModel){
-        if (this.userService.authenticate(userLoginBindingModel.getUsername(),userLoginBindingModel.getPassword())) {
-            this.userService.loginUser(userLoginBindingModel.getUsername());
-            return "redirect:/";
-        }else {
+    public String loginConfirm(UserLoginBindingModel userLoginBindingModel,
+                               RedirectAttributes redirectAttributes) {
+
+
+        boolean authenticate = this.userService.authenticate(userLoginBindingModel.getUsername(),
+                userLoginBindingModel.getPassword());
+        if (!authenticate) {
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("notFound", true);
+
             return "redirect:login";
         }
-    }
 
+        this.userService.loginUser(userLoginBindingModel.getUsername());
+        return "redirect:/";
+    }
 }
